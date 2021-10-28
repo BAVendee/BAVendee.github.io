20b387a1ad03d972bd50709cd9a47693053ddba3
BAVendee <92738048+BAVendee@users.noreply.github.com>
2021-10-28 15:08:24 +0200

    Add files via upload

---
+++ qgis2web_2021_10_28-14_59_14_900919/styles/parStVincentdePaul_6_style.js
@@ -0,0 +1,54 @@
+var size = 0;
+var placement = 'point';
+
+var style_parStVincentdePaul_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = '';
+    var labelText = "";
+    size = 0;
+    var labelFont = "7.800000000000001px \'Helvetica\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("nom") !== null) {
+        labelText = String(feature.get("nom"));
+    }
+    
+        function rules_parStVincentdePaul_6(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_parStVincentdePaul_6rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(178,223,138,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+                    else if (exp_parStVincentdePaul_6rule1_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(178,223,138,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_parStVincentdePaul_6(feature, value);
+        ;
+
+    return style;
+};
