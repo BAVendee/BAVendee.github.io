77e2d0037e37ed7b31ef71070704e9a9b3d505f2
BAVendee <92738048+BAVendee@users.noreply.github.com>
2021-11-28 17:44:34 +0100

    Add files via upload

---
+++ qgis2web_2021_11_22-17_44_03_230825/styles/parEpiceriesdescommunes_5_style.js
@@ -0,0 +1,54 @@
+var size = 0;
+var placement = 'point';
+
+var style_parEpiceriesdescommunes_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = '';
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.4px \'Helvetica\', sans-serif";
+    var labelFill = "#e31a1c";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("aide_alimentaire_asso_maires_nom_9") !== null) {
+        labelText = String(feature.get("aide_alimentaire_asso_maires_nom_9"));
+    }
+    
+        function rules_parEpiceriesdescommunes_5(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_parEpiceriesdescommunes_5rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(250,255,57,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+                    else if (exp_parEpiceriesdescommunes_5rule1_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(249,250,170,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_parEpiceriesdescommunes_5(feature, value);
+        ;
+
+    return style;
+};
